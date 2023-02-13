bf295d637aaa6bb61f42719411f4aa6c90595225
Flavio <fjcf@poli.br>
2023-02-13 15:12:12 -0300

    feat: add register to routes

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import fastify from 'fastify'
-import { knexDatabase } from './database'
-import crypto from 'node:crypto'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knexDatabase('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Transação de teste',
-    amount: 1000
-  }).returning('*')
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
